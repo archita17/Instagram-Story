aca1a7c691061d8cd4994004160c7dc8c1e7a851
Vartika Singh <vartikasingh@Vartikas-MacBook-Air.local>
2025-04-05 17:25:38 +0530

    image converted to base64, stored in localstorage and available for preview

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Instagram-Story
+
+
+//1. story bar similar to a navbar - archi
+//2. plus button - archi
+//3. upload an image > converted to base64 > store in local storage - 
+//4. when user clicks on a story
+    //4.1 story will show for 3 seconds with a progress bar at top
+    //4.2 user can switch story by swiping
+    //4.3 seperate progress bar for each story 
+    //4.4 progress bar should fill up when 3 seconds are over
+//5. the story should be removed automatically from local sotrage after 24hrs
+
+//constraints
+//The project should be responsive
+//Image dimensions should be constrained to a maximum of 1080px x 1920px
--- index.js
+++ index.js
@@ -1,18 +1,42 @@
 function upload_Image() {
-  const popup = document.createElement("div");
-  popup.classList.add("popup");
+    const popup = document.createElement("div");
+    popup.classList.add("popup");
 
-  popup.innerHTML = `
+    popup.innerHTML = `
       <div class="popup-content">
         <span class="close-btn">&times;</span>
         <h2>Upload Image</h2>
-        <input type="file" accept="image/*" />
+        <input type="file" accept="image/*" id="upload-an-image"/>
+        <img id="preview" style="max-width: 200px; margin-top: 10px;" />
       </div>
     `;
 
-  document.body.appendChild(popup);
+    document.body.appendChild(popup);
+    let count = 0;
+    setTimeout(() => {
+        const getUploadedImage = document.getElementById("upload-an-image");
+        const showUploadedImage = document.getElementById("preview");
 
-  popup.querySelector(".close-btn").onclick = () => {
-    popup.remove();
-  };
+        getUploadedImage.addEventListener("change", function () {
+            const file = this.files[0];
+            const reader = new FileReader();
+
+            reader.onload = function (event) {
+                const image_base64 = event.target.result;
+                count++;
+                localStorage.setItem(`Story_${count}`, image_base64);
+                showUploadedImage.src = image_base64;
+            };
+
+            if (file) {
+                reader.readAsDataURL(file);
+            }
+        });
+
+    }, 100);
+
+
+    popup.querySelector(".close-btn").onclick = () => {
+        popup.remove();
+    };
 }
